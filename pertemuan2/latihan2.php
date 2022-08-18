fb03ad8fc3c3405d0b8808007f3e955259965c98
Trysked <trysked@gmail.com>
2022-08-18 12:25:29 +0700

    belajar pengulangan dan aritmatika serta operator

---
+++ pertemuan2/latihan2.php
@@ -0,0 +1,18 @@
+<?php 
+// pengkondisian / percabangan
+// if else
+// if else if else
+// ternary 
+// switch
+
+
+$x = 0;
+if($x < 20) {
+   echo "benar";
+} else if ($x == 20) {
+   echo "ding dong!!";
+}
+else {
+   echo "salah";
+}
+?>
