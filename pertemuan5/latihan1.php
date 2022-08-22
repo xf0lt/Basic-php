38c60f34c4deb83d2ab3f02d899e05e97e5db939
Trysked <trysked@gmail.com>
2022-08-22 11:16:45 +0700

    memahami bagaimana mengubah dan mengambil data dari dalam array yang memiliki array

--- pertemuan5/latihan1.php
+++ pertemuan5/latihan1.php
@@ -1,8 +1,41 @@
-<?php 
-// Array
-// Membuat Array
-$hari = array("Senin", "Selasa", "Rabu");
-$bulan = ["Januari", "Februari", "Maret"];
-$arr = [100, "tesk", true];
-// menampilkan array
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Latihan Array</title>
+   <style>
+      .kotak {
+         width: 30px;
+         height: 30px;
+         background-color: salmon;
+         text-align: center;
+         line-height: 30px;
+         margin: 3px;
+         float: left;
+         transition: 1s;
+      }
+      .kotak:hover {
+         transform: rotate(360deg);
+         border-radius: 50%;
+      }
+      .clear {
+         clear: both;
+      }
+   </style>
+</head>
+<body>
+   <?php 
+      $angka = [[1,2,3],[4,5,6],[7,8,9]];
+      // echo $angka[2][2];
+   ?>
+   
+   <?php foreach( $angka as $a ) : ?>
+      <?php foreach($a as $b) : ?>
+         <div class="kotak"><?= $b ?></div>
+      <?php endforeach; ?>
+      <div class="clear"></div>
+   <?php endforeach; ?>
+</body>
+</html>
